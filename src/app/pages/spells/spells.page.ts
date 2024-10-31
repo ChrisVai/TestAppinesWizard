bf42f584def71d582caf9ba59c1e0127510814e3
ChrisVai <chrisbellet@gmail.com>
2024-10-31 19:07:36 +0100

    Sauvegarde de fin de journée

--- src/app/pages/spells/spells.page.ts
+++ src/app/pages/spells/spells.page.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, computed, inject, signal, Signal, WritableSignal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {IonContent, IonHeader, IonSearchbar, IonTitle, IonToolbar} from '@ionic/angular/standalone';
-import {HttpClient} from "@angular/common/http";
 import {SpellCardComponent} from "../../components/spell-card/spell-card.component";
 import {Spell} from "../../models/spell";
+import {SpellService} from "../../services/spell.service";
 
 @Component({
   selector: 'app-spells',
@@ -13,32 +13,19 @@ import {Spell} from "../../models/spell";
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, SpellCardComponent, IonSearchbar]
 })
-export class SpellsPage implements OnInit {
+export class SpellsPage {
 
   // TODO : use signal
-  spells: Spell[] = [];
+  spells: Signal<Spell[]> = inject(SpellService).fetchSpells();
 
   // TODO : use signal
-  searchInput: string = '';
+  searchInput: WritableSignal<string> = signal('');
 
   // TODO : use computed signal
-  protected filteredSpells: Spell[] = [];
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit() {
-    this.fetchSpells();
-  }
-
-  fetchSpells() {
-    // TODO : move in a specific service
-    this.http.get<Spell[]>('https://hp-api.onrender.com/api/spells').subscribe((res: Spell[]) => {
-      this.spells = res;
-    })
-  }
-
-  filterSpell() {
-    this.filteredSpells = this.spells.filter(spell => spell.name.toLowerCase().includes(this.searchInput.toLowerCase() || ''))
-  }
-
+  protected filteredSpells: Signal<Spell[]> = computed(
+    () => this.spells().filter(
+      spell => spell.name.toLowerCase().includes(this.searchInput().toLowerCase() || ''
+      )
+    )
+  );
 }
