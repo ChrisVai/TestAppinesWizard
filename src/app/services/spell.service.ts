94d27f81aa10f0a339ecd2745030645dd545e952
ChrisVai <chrisbellet@gmail.com>
2024-10-31 19:05:56 +0100

    Adding SpellService

---
+++ src/app/services/spell.service.ts
@@ -0,0 +1,13 @@
+import {inject, Injectable, Signal} from '@angular/core';
+import {Spell} from "../models/spell";
+import {HttpClient} from "@angular/common/http";
+import {toSignal} from "@angular/core/rxjs-interop";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpellService {
+  fetchSpells() {
+    return toSignal(inject(HttpClient).get<Spell[]>('https://hp-api.onrender.com/api/spells'), {initialValue: []});
+  }
+}
