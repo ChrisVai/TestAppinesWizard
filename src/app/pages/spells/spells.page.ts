68e2fd84030e0cda550da1bbd010d657d488d243
ChrisVai <chrisbellet@gmail.com>
2024-11-01 18:53:28 +0100

    Update Spells with new Control Flow & Signals

--- src/app/pages/spells/spells.page.ts
+++ src/app/pages/spells/spells.page.ts
@@ -1,31 +1,48 @@
-import {Component, computed, inject, signal, Signal, WritableSignal} from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  signal,
+  Signal,
+  WritableSignal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonContent, IonHeader, IonSearchbar, IonTitle, IonToolbar} from '@ionic/angular/standalone';
-import {SpellCardComponent} from "../../components/spell-card/spell-card.component";
-import {Spell} from "../../models/spell";
-import {SpellService} from "../../services/spell.service";
+import {
+  IonContent,
+  IonHeader,
+  IonSearchbar,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { SpellCardComponent } from '../../components/spell-card/spell-card.component';
+import { Spell } from '../../models/spell';
+import { SpellService } from '../../services/spell.service';
 
 @Component({
   selector: 'app-spells',
   templateUrl: './spells.page.html',
   styleUrls: ['./spells.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, SpellCardComponent, IonSearchbar]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    SpellCardComponent,
+    IonSearchbar,
+  ],
 })
 export class SpellsPage {
+  private readonly spells: Signal<Spell[]> = inject(SpellService).fetchSpells();
 
-  // TODO : use signal
-  spells: Signal<Spell[]> = inject(SpellService).fetchSpells();
-
-  // TODO : use signal
   searchInput: WritableSignal<string> = signal('');
 
-  // TODO : use computed signal
-  protected filteredSpells: Signal<Spell[]> = computed(
-    () => this.spells().filter(
-      spell => spell.name.toLowerCase().includes(this.searchInput().toLowerCase() || ''
-      )
-    )
+  protected filteredSpells: Signal<Spell[]> = computed(() =>
+    this.spells().filter(spell =>
+      spell.name.toLowerCase().includes(this.searchInput().toLowerCase()),
+    ),
   );
 }
