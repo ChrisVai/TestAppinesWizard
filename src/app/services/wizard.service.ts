08d1cadb8dd5c5a1a7f40e2b7c01af9d41437cc3
ChrisVai <chrisbellet@gmail.com>
2024-11-04 12:18:06 +0100

    Update WizardService

--- src/app/services/wizard.service.ts
+++ src/app/services/wizard.service.ts
@@ -3,17 +3,42 @@ import { Spell } from '../models/spell';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { HttpClient } from '@angular/common/http';
 import { Wizard } from '../models/wizard';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WizardService {
+  private readonly http: HttpClient = inject(HttpClient);
   fetchWizards(): Signal<Wizard[]> {
     return toSignal(
-      inject(HttpClient).get<Wizard[]>(
-        'https://hp-api.onrender.com/api/characters',
-      ),
+      this.http
+        .get<Wizard[]>('https://hp-api.onrender.com/api/characters')
+        .pipe(
+          map((wizards: Wizard[]) => {
+            // used to truncate base URL and using provideImageKitLoader() for NgOptimizedImage's preload
+            for (const wizard of wizards) {
+              wizard.image = wizard.image.slice(28);
+            }
+            return wizards;
+          }),
+        ),
       { initialValue: [] },
     );
+  }
+
+  fetchWizardById(id: string): Observable<Wizard> {
+    return this.http
+      .get<Wizard[]>(`https://hp-api.onrender.com/api/character/${id}`)
+      .pipe(
+        map((wizards: Wizard[]) => {
+          // used to truncate base URL and using provideImageKitLoader() for NgOptimizedImage's preload
+          for (const wizard of wizards) {
+            wizard.image = wizard.image.slice(28);
+          }
+          // api call return only one wizard as array
+          return wizards[0];
+        }),
+      );
   }
 }
